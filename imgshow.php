ef782f31edd3e4f7b16a36785508a053b1b1a66f
Ali <ali.nematollahi2020@gmail.com>
2021-02-05 14:47:34 +0100

    Add imgshow.php

---
+++ imgshow.php
@@ -0,0 +1,72 @@
+<?php
+    session_start();
+/*
+    $_SESSION['img1'];
+    $_SESSION['filesrc1'];
+    $_SESSION['img2'];
+    $_SESSION['filesrc2'];
+
+    $_SESSION['img3'];
+    $_SESSION['filesrc3'];
+*/
+
+
+
+   if (!file_exists("temp")) {
+    mkdir("temp");
+    }
+
+    if (isset($_FILES['img1'])) {
+    
+        $imgName = $_FILES['img1']['name'];
+        $array = explode(".",$imgName );
+        $ext = end($array);
+        $newName = rand().".".$ext;
+
+        $from = $_FILES['img1']['tmp_name'];
+        $to = "temp"."/".$newName;
+        move_uploaded_file($from,$to);
+
+        $_SESSION['img1'] = 'ok';
+        $_SESSION['filesrc1'] = $to;
+        header("location:management.php");
+        
+    }
+
+    if (isset($_FILES['img2'])) {
+    
+        $imgName = $_FILES['img2']['name'];
+        $array = explode(".",$imgName );
+        $ext = end($array);
+        $newName = rand().".".$ext;
+
+        $from = $_FILES['img2']['tmp_name'];
+        $to = "temp"."/".$newName;
+        move_uploaded_file($from,$to);
+
+        $_SESSION['img2'] = 'ok';
+        $_SESSION['filesrc2'] = $to;
+        header("location:management.php");
+    }
+
+    if (isset($_FILES['img3'])) {
+    
+        $imgName = $_FILES['img3']['name'];
+        $array = explode(".",$imgName );
+        $ext = end($array);
+        $newName = rand().".".$ext;
+
+        $from = $_FILES['img3']['tmp_name'];
+        $to = "temp"."/".$newName;
+        move_uploaded_file($from,$to);
+
+        $_SESSION['img3'] = 'ok';
+        $_SESSION['filesrc3'] = $to;
+        header("location:management.php");
+    }
+
+    //header("location:management.php?img=ok&filesrc=".$to);
+
+    //header("location:management.php?img=ok&filesrc=".$to);
+
+?>
