99c49da2b3984082ce696d8349bfe38e01a2c9d3
Ali <ali.nematollahi2020@gmail.com>
2021-02-14 18:48:04 +0100

    update

--- imgPreview.php
+++ imgPreview.php
@@ -1,40 +1,117 @@
 <?php
 
-if (isset($_FILES['imgPreview'])) {
+//===================================  Image 1 preview  ===================================//
+
+// add image
+if (isset($_FILES['img1Preview'])) {
     
-    $imgName = $_FILES['imgPreview']['name'];
+    $imgName = $_FILES['img1Preview']['name'];
     $array = explode(".",$imgName );
     $ext = end($array);
     $newName = rand().".".$ext;
 
-    $from = $_FILES['imgPreview']['tmp_name'];
+    $from = $_FILES['img1Preview']['tmp_name'];
     $to = "temp"."/".$newName;
     move_uploaded_file($from,$to);
     echo $to;
 }
 
+// change image
+if (isset($_FILES['img1PreviewChange']) && isset($_POST['img1PreviewRemove'])) {
 
-
-if (isset($_FILES['imgPreviewChange']) && isset($_POST['removePreviousImg'])) {
-
-    $removeImg = $_POST['removePreviousImg'];
+    $removeImg = $_POST['img1PreviewRemove'];
     unlink($removeImg);
     
-    $imgName = $_FILES['imgPreviewChange']['name'];
+    $imgName = $_FILES['img1PreviewChange']['name'];
     $array = explode(".",$imgName );
     $ext = end($array);
     $newName = rand().".".$ext;
 
-    $from = $_FILES['imgPreviewChange']['tmp_name'];
+    $from = $_FILES['img1PreviewChange']['tmp_name'];
     $to = "temp"."/".$newName;
     move_uploaded_file($from,$to);
     echo $to;
 }
 
-if (!isset($_FILES['imgPreviewChange']) && isset($_POST['removePreviousImg'])) {
-    unlink($_POST['removePreviousImg']);
+// remove image
+if (!isset($_FILES['img1PreviewChange']) && isset($_POST['img1PreviewRemove'])) {
+    unlink($_POST['img1PreviewRemove']);
 }
 
+//===================================  Image 2 preview  ===================================//
 
+// add image
+if (isset($_FILES['img2Preview'])) {
+    
+    $imgName = $_FILES['img2Preview']['name'];
+    $array = explode(".",$imgName );
+    $ext = end($array);
+    $newName = rand().".".$ext;
+
+    $from = $_FILES['img2Preview']['tmp_name'];
+    $to = "temp"."/".$newName;
+    move_uploaded_file($from,$to);
+    echo $to;
+}
+
+// change image
+if (isset($_FILES['img2PreviewChange']) && isset($_POST['img2PreviewRemove'])) {
+
+    $removeImg = $_POST['img2PreviewRemove'];
+    unlink($removeImg);
+    
+    $imgName = $_FILES['img2PreviewChange']['name'];
+    $array = explode(".",$imgName );
+    $ext = end($array);
+    $newName = rand().".".$ext;
+
+    $from = $_FILES['img2PreviewChange']['tmp_name'];
+    $to = "temp"."/".$newName;
+    move_uploaded_file($from,$to);
+    echo $to;
+}
+
+// remove image
+if (!isset($_FILES['img2PreviewChange']) && isset($_POST['img2PreviewRemove'])) {
+    unlink($_POST['img2PreviewRemove']);
+}
+
+//===================================  Image 3 preview  ===================================//
+
+// add image
+if (isset($_FILES['img3Preview'])) {
+    
+    $imgName = $_FILES['img3Preview']['name'];
+    $array = explode(".",$imgName );
+    $ext = end($array);
+    $newName = rand().".".$ext;
+
+    $from = $_FILES['img3Preview']['tmp_name'];
+    $to = "temp"."/".$newName;
+    move_uploaded_file($from,$to);
+    echo $to;
+}
+
+// change image
+if (isset($_FILES['img3PreviewChange']) && isset($_POST['img3PreviewRemove'])) {
+
+    $removeImg = $_POST['img3PreviewRemove'];
+    unlink($removeImg);
+    
+    $imgName = $_FILES['img3PreviewChange']['name'];
+    $array = explode(".",$imgName );
+    $ext = end($array);
+    $newName = rand().".".$ext;
+
+    $from = $_FILES['img3PreviewChange']['tmp_name'];
+    $to = "temp"."/".$newName;
+    move_uploaded_file($from,$to);
+    echo $to;
+}
+
+// remove image
+if (!isset($_FILES['img3PreviewChange']) && isset($_POST['img3PreviewRemove'])) {
+    unlink($_POST['img3PreviewRemove']);
+}
 
 ?>
