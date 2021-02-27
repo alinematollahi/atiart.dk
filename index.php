525db163f807adcf6de3e79242d2787378c5adf0
Ali <ali.nematollahi2020@gmail.com>
2021-02-27 10:17:23 +0100

    create index.php (main page)

---
+++ index.php
@@ -0,0 +1,349 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AtiArt - Quilling</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/6dba727b51.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div id="home-header"></div>
+    <div id="menu">
+        <button id="homePageBtn" type="button">Home</button>
+        <button id="shopPageBtn" type="button">Shop</button>
+        <button id="aboutPageBtn" type="button">About AtiArt</button>
+        <button id="contactPageBtn" type="button">Contact Us</button>
+        <button id="cartPageBtn" type="button">Cart <i class="fas fa-shopping-cart"></i></button>
+    </div>
+    <div id="homePage">
+        <div id="content-part1">
+            Quilling <br><br>
+            Quilling is the art of rolled, shaped,
+            and glued paper that results in creating
+            a unified, decorative design. The name
+            quilling is thought to come from the origin
+            of the art; birds’ feathers, or quills, were
+            used to coil the strips of paper around.
+        </div>
+    
+        <div id="content-part2">
+            <div id="sample1">
+                <a class="link" href="sample1.html"></a>
+                <div class="detail">
+                    price : 700 DKK
+                    <br>
+                    size : 30cm * 23cm
+                    <br>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+            <div id="sample2">
+                <a class="link" href="sample2.html"></a>
+                <div class="detail">
+                    price : 700 DKK
+                    <br>
+                    size : 30cm * 23cm
+                    <br>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+            <div id="sample3">
+                <a class="link" href="sample3.html"></a>
+                <div class="detail">
+                    price : 700 DKK
+                    <br>
+                    size : 30cm * 23cm
+                    <br>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+            <div id="sample4">
+                <a class="link" href="sample4.html"></a>
+                <div class="detail">
+                    price : 700 DKK
+                    <br>
+                    size : 30cm * 23cm
+                    <br>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+            <div id="sample5">
+                <a class="link" href="sample5.html"></a>
+                <div class="detail">
+                    price : 700 DKK
+                    <br>
+                    size : 30cm * 23cm
+                    <br>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+            <div id="sample6">
+                <a class="link" href="sample6.html"></a>
+                <div class="detail">
+                    price : 700 DKK
+                    <br>
+                    size : 30cm * 23cm
+                    <br>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+            <div id="sample7">
+                <a class="link" href="sample7.html"></a>
+                <div class="detail">
+                    price : 700 DKK
+                    <br>
+                    size : 30cm * 23cm
+                    <br>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+            <div id="sample8">
+                <a class="link" href="sample8.html"></a>
+                <div class="detail">
+                    price : 700 DKK
+                    <br>
+                    size : 30cm * 23cm
+                    <br>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+            <div id="sample9">
+                <a class="link" href="sample9.html"></a>
+                <div class="detail">
+                    price : 700 DKK
+                    <br>
+                    size : 30cm * 23cm
+                    <br>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+            <div id="sample10">
+                <a class="link" href="sample10.html"></a>
+                <div class="detail">
+                    price : 700 DKK
+                    <br>
+                    size : 30cm * 23cm
+                    <br>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+            <div id="sample11">
+                <a class="link" href="sample11.html"></a>
+                <div class="detail">
+                    price : 700 DKK
+                    <br>
+                    size : 30cm * 23cm
+                    <br>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+            <div id="sample12">
+                <a class="link" href="sample12.html"></a>
+                <div class="detail">
+                    price : 700 DKK
+                    <br>
+                    size : 30cm * 23cm
+                    <br>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+        </div>
+    
+        <div id="content-tail">
+            <a href="shop.html">
+                SEE MORE
+            </a>
+        </div>
+    </div>
+    <div id="shopPage"></div>
+    <div id="aboutPage">aboutPage</div>
+    <div id="contactPage">
+        <div id="contact-part1">
+            CONTACT US
+        </div>
+        <div id="contact-part2">
+            <ul>
+                <li>
+                    <i class="fas fa-map-marker-alt"></i>
+                    Copenhagen-Denmark
+                </li>
+                <li>
+                    <i class="fas fa-phone-square-alt"></i>
+                    +45 71557785
+                </li>
+                <li>
+                    <i class="fas fa-envelope"></i>
+                    atiart@gmail.com
+                </li>
+                <li>
+                    <a href="https://www.facebook.com/ati.art.165" target="_blank">
+                        <i class="fab fa-facebook-square"></i>
+                        ati.art.165
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/handmade.denmark" target="_blank">
+                        <i class="fab fa-instagram"></i>
+                        handmade.denmark
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div id="contact-part3">
+            <form action="getmessage.php">
+                <div class="required">
+                    <input type="text" placeholder="your Name" required id="i1" name="frm[userName]">
+                </div>
+                <div class="required">
+                    <input type="email" placeholder="your Email" required name="frm[userEmail]">
+                </div>
+                <div class="required">
+                    <textarea cols="30" rows="6" placeholder="your message" required name="frm[userMessage]"></textarea>
+                </div>
+                <div>
+                    <input type="submit" value="send message">
+                </div>
+            </form>
+        </div>
+    </div>
+    <div id="cartPage">
+    <div class="cart-box">
+        <div class="cart-box-part1"></div>
+        <div class="cart-box-part2">
+            <table class="cart-box-part2-table1">
+                <tr>
+                    <td class="cart-box-part2-table1-col1">Name</td>
+                    <td class="cart-box-part2-table1-col2"></td>
+                </tr>
+                <tr>
+                    <td class="cart-box-part2-table1-col1">Unit Price</td>
+                    <td class="cart-box-part2-table1-col2"> 755 DKK</td>
+                </tr>
+            </table>
+
+            <table class="cart-box-part2-table2">
+                <tr>
+                    <td class="cart-box-part2-table2-col1">Quantity</td>
+                    <td class="cart-box-part2-table2-col2">
+                        <input type="number" class="quantity" placeholder="1">
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div class="cart-box-part3"> 755 DKK</div>
+        <div class="cart-box-part4">
+            <i class="fas fa-times"></i>
+        </div>
+    </div>
+
+    <div id="invoice">
+        <div>
+            <h2> Order Summary </h2>
+        </div>
+        <div>
+
+            <table>
+                <tr>
+                    <td> Subtotal</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Tax (5%)</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Shipping</td>
+                    <td></td>
+                </tr>
+            </table>
+
+            <table>
+                <tr>
+                    <td id="total">Total</td>
+                    <td></td>
+                </tr>
+            </table>
+
+        </div>
+
+        <div>
+            <button>Checkout</button>
+        </div>
+    </div>
+    </div>
+    <div id="sample-container">
+        <div id="sample-part1">
+            <div class="sample-part1-inside" id="sampleimg1" ></div>
+            <div class="sample-part1-inside" id="sampleimg2" ></div>
+            <div class="sample-part1-inside" id="sampleimg3" ></div>
+        </div>
+        <div id="sample-part2">
+
+        </div>
+        <div id="sample-part3">
+            <table>
+                <tr>
+                    <td class="col1">Name</td>
+                    <td class="col2" id="nameValue"></td>
+                </tr>
+                <tr>
+                    <td class="col1">Size</td>
+                    <td class="col2" id="sizeValue"></td>
+                </tr>
+                <tr>
+                    <td class="col1">Price</td>
+                    <td class="col2" id="priceValue"></td>
+                </tr>
+                <tr>
+                    <td class="col1">Details</td>
+                    <td class="col2" id="detailsValue"></td>
+                </tr>
+            </table>
+            <div>
+                <button type="button">Add to Cart</button>
+                <button type="button" id="ReturntoShopPageBtn" >Return to Shop Page</button>
+            </div>
+        </div>
+    </div>
+    <div id="footer">
+        <div>
+            Quilling Art <br> <br>
+            Quilling is the art of rolled,
+            shaped, and glued paper that results
+            in creating a unified,
+            decorative design.
+        </div>
+        <div>
+            Contact <br><br>
+            <i class="fas fa-map-marker-alt"></i>
+            Copenhagen-Denmark
+            <br>
+            <i class="fas fa-phone-square-alt"></i>
+            +45 71557785
+            <br>
+            <a href="https://www.facebook.com/ati.art.165" target="_blank">
+                <i class="fab fa-facebook-square"></i>
+                ati.art.165
+            </a>
+            <br>
+            <a href="https://www.instagram.com/handmade.denmark" target="_blank">
+                <i class="fab fa-instagram"></i>
+                handmade.denmark
+            </a>
+        </div>
+        <div>
+            Ati Art
+            <br>
+            <ul>
+                <li>All in the site is handmade</li>
+                <li>suitable as cute decorations and lovely gifts</li>
+                <li>Custom orders are welcome</li>
+                <li>Shipping to all over the world</li>
+            </ul>
+        </div>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+</html>
