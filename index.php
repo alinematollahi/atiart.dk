92df5c47c28cfa658982ab90d765765ca21496ae
Ali <ali.nematollahi2020@gmail.com>
2021-02-28 21:58:57 +0100

    set cart

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,8 +8,10 @@
     <link rel="stylesheet" href="style.css">
     <script src="https://kit.fontawesome.com/6dba727b51.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
     <div id="home-header"></div>
+
     <div id="menu">
         <button id="homePageBtn" type="button">Home</button>
         <button id="shopPageBtn" type="button">Shop</button>
@@ -16,6 +19,7 @@
         <button id="contactPageBtn" type="button">Contact Us</button>
         <button id="cartPageBtn" type="button">Cart <i class="fas fa-shopping-cart"></i></button>
     </div>
+
     <div id="homePage">
         <div id="content-part1">
             Quilling <br><br>
@@ -26,138 +30,25 @@
             of the art; birds’ feathers, or quills, were
             used to coil the strips of paper around.
         </div>
-    
-        <div id="content-part2">
-            <div id="sample1">
-                <a class="link" href="sample1.html"></a>
-                <div class="detail">
-                    price : 700 DKK
-                    <br>
-                    size : 30cm * 23cm
-                    <br>
-                    <button>Add to Cart</button>
-                </div>
-            </div>
-            <div id="sample2">
-                <a class="link" href="sample2.html"></a>
-                <div class="detail">
-                    price : 700 DKK
-                    <br>
-                    size : 30cm * 23cm
-                    <br>
-                    <button>Add to Cart</button>
-                </div>
-            </div>
-            <div id="sample3">
-                <a class="link" href="sample3.html"></a>
-                <div class="detail">
-                    price : 700 DKK
-                    <br>
-                    size : 30cm * 23cm
-                    <br>
-                    <button>Add to Cart</button>
-                </div>
-            </div>
-            <div id="sample4">
-                <a class="link" href="sample4.html"></a>
-                <div class="detail">
-                    price : 700 DKK
-                    <br>
-                    size : 30cm * 23cm
-                    <br>
-                    <button>Add to Cart</button>
-                </div>
-            </div>
-            <div id="sample5">
-                <a class="link" href="sample5.html"></a>
-                <div class="detail">
-                    price : 700 DKK
-                    <br>
-                    size : 30cm * 23cm
-                    <br>
-                    <button>Add to Cart</button>
-                </div>
-            </div>
-            <div id="sample6">
-                <a class="link" href="sample6.html"></a>
-                <div class="detail">
-                    price : 700 DKK
-                    <br>
-                    size : 30cm * 23cm
-                    <br>
-                    <button>Add to Cart</button>
-                </div>
-            </div>
-            <div id="sample7">
-                <a class="link" href="sample7.html"></a>
-                <div class="detail">
-                    price : 700 DKK
-                    <br>
-                    size : 30cm * 23cm
-                    <br>
-                    <button>Add to Cart</button>
-                </div>
-            </div>
-            <div id="sample8">
-                <a class="link" href="sample8.html"></a>
-                <div class="detail">
-                    price : 700 DKK
-                    <br>
-                    size : 30cm * 23cm
-                    <br>
-                    <button>Add to Cart</button>
-                </div>
-            </div>
-            <div id="sample9">
-                <a class="link" href="sample9.html"></a>
-                <div class="detail">
-                    price : 700 DKK
-                    <br>
-                    size : 30cm * 23cm
-                    <br>
-                    <button>Add to Cart</button>
-                </div>
-            </div>
-            <div id="sample10">
-                <a class="link" href="sample10.html"></a>
-                <div class="detail">
-                    price : 700 DKK
-                    <br>
-                    size : 30cm * 23cm
-                    <br>
-                    <button>Add to Cart</button>
-                </div>
-            </div>
-            <div id="sample11">
-                <a class="link" href="sample11.html"></a>
-                <div class="detail">
-                    price : 700 DKK
-                    <br>
-                    size : 30cm * 23cm
-                    <br>
-                    <button>Add to Cart</button>
-                </div>
-            </div>
-            <div id="sample12">
-                <a class="link" href="sample12.html"></a>
-                <div class="detail">
-                    price : 700 DKK
-                    <br>
-                    size : 30cm * 23cm
-                    <br>
-                    <button>Add to Cart</button>
-                </div>
-            </div>
-        </div>
-    
+        <h2>Sample Products</h2>
+        <div id="content-part2"></div>
+
         <div id="content-tail">
-            <a href="shop.html">
-                SEE MORE
-            </a>
+            <button type="button" id="homeSeeMoreBtn"> See More</button>
         </div>
     </div>
+
     <div id="shopPage"></div>
-    <div id="aboutPage">aboutPage</div>
+
+    <div id="aboutPage">
+            <ul>
+                <li>All in the site is handmade</li>
+                <li>suitable as cute decorations and lovely gifts</li>
+                <li>Custom orders are welcome</li>
+                <li>Shipping to all over the world</li>
+            </ul>        
+    </div>
+
     <div id="contactPage">
         <div id="contact-part1">
             CONTACT US
@@ -207,76 +98,52 @@
             </form>
         </div>
     </div>
+
     <div id="cartPage">
-    <div class="cart-box">
-        <div class="cart-box-part1"></div>
-        <div class="cart-box-part2">
-            <table class="cart-box-part2-table1">
-                <tr>
-                    <td class="cart-box-part2-table1-col1">Name</td>
-                    <td class="cart-box-part2-table1-col2"></td>
-                </tr>
-                <tr>
-                    <td class="cart-box-part2-table1-col1">Unit Price</td>
-                    <td class="cart-box-part2-table1-col2"> 755 DKK</td>
-                </tr>
-            </table>
 
-            <table class="cart-box-part2-table2">
-                <tr>
-                    <td class="cart-box-part2-table2-col1">Quantity</td>
-                    <td class="cart-box-part2-table2-col2">
-                        <input type="number" class="quantity" placeholder="1">
-                    </td>
-                </tr>
-            </table>
+        <div id="invoice">
+            <div>
+                <h2> Order Summary </h2>
+            </div>
+            <div>
+
+                <table>
+                    <tr>
+                        <td> Subtotal</td>
+                        <td id="subtotal"></td>
+                    </tr>
+                    <tr>
+                        <td>Tax (5%)</td>
+                        <td id="tax"></td>
+                    </tr>
+                    <tr>
+                        <td>Shipping</td>
+                        <td id="shipping"></td>
+                    </tr>
+                </table>
+
+                <table>
+                    <tr>
+                        <td id="total-topic">Total</td>
+                        <td id="total"></td>
+                    </tr>
+                </table>
+
+            </div>
+
+            <div>
+                <button>Checkout</button>
+            </div>
         </div>
-        <div class="cart-box-part3"> 755 DKK</div>
-        <div class="cart-box-part4">
-            <i class="fas fa-times"></i>
-        </div>
+
+        <div  id="cart-box-container"></div>
     </div>
 
-    <div id="invoice">
-        <div>
-            <h2> Order Summary </h2>
-        </div>
-        <div>
-
-            <table>
-                <tr>
-                    <td> Subtotal</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Tax (5%)</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Shipping</td>
-                    <td></td>
-                </tr>
-            </table>
-
-            <table>
-                <tr>
-                    <td id="total">Total</td>
-                    <td></td>
-                </tr>
-            </table>
-
-        </div>
-
-        <div>
-            <button>Checkout</button>
-        </div>
-    </div>
-    </div>
     <div id="sample-container">
         <div id="sample-part1">
-            <div class="sample-part1-inside" id="sampleimg1" ></div>
-            <div class="sample-part1-inside" id="sampleimg2" ></div>
-            <div class="sample-part1-inside" id="sampleimg3" ></div>
+            <div class="sample-part1-inside" id="sampleimg1"></div>
+            <div class="sample-part1-inside" id="sampleimg2"></div>
+            <div class="sample-part1-inside" id="sampleimg3"></div>
         </div>
         <div id="sample-part2">
 
@@ -300,12 +167,24 @@
                     <td class="col2" id="detailsValue"></td>
                 </tr>
             </table>
-            <div>
-                <button type="button">Add to Cart</button>
-                <button type="button" id="ReturntoShopPageBtn" >Return to Shop Page</button>
+            <div id="sampleInputContainer">
+                <input type="hidden">
+                <input type="hidden">
+                <input type="hidden">
+                <input type="hidden">
+                <input type="hidden">
+                <input type="hidden">
+                <input type="hidden">
+                <input type="hidden">
+                <div>
+                    <button type="button" onclick="addToCart(event)">Add to Cart</button>
+                    <button type="button" id="ReturntoShopPageBtn">Return to Shop Page</button>
+                </div>  
             </div>
         </div>
     </div>
+
+
     <div id="footer">
         <div>
             Quilling Art <br> <br>
@@ -344,6 +223,16 @@
         </div>
     </div>
 
+    <div id="hide-window">
+        <div id="cart-question" class="question">
+            <div id="addedText"> Product Added to the Cart</div>
+            <div id="existText"> This Product Is Already in the Cart</div>
+            <button id="backToShop"> Continue Shopping </button>
+            <button id="seeCart"> See the Cart </button>
+        </div>
+    </div>
+
     <script src="script.js"></script>
 </body>
+
 </html>
